3965352b4ad5ab15c07aec8f5d466107afb6b323
Abhay Pandey <pandeyabhay055@gmail.com>
2026-07-11 22:17:10 +0530

    hooks overview

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+function App() {
+
+  //  let counter = 0;
+  const [counter, setCounter] = useState(0)
+
+
+  const addValue = () => {
+    setCounter(counter + 1)
+    console.log(counter)
+  }
+
+  const counterRemoveValue = () => {
+    if (counter == 0) {
+      counter = 0;
+    } else {
+      setCounter(counter - 1)
+    }
+  }
+
+
+  return (
+    <>
+      <h1>Counter Is Here</h1>
+      <h3>Counter Value is: {counter}</h3>
+
+      <button onClick={addValue} >Add Value</button>
+      <br />
+      <button onClick={counterRemoveValue} >Remove Value</button>
+
+    </>
+  )
+}
+
+export default App
